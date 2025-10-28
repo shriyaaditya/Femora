e27996fec1bc41f8bfdc4ac2cbcc2cb5386eb4b8
Vivek Sawant <vsawantlm17@gmail.com>
2025-10-28 09:16:48 +0530

    Fix Android bundling issues: Update to SDK 54, fix babel plugins, and optimize project size

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,6 @@
 module.exports = function (api) {
   api.cache(true);
-  let plugins = [];
+  let plugins = ['react-native-worklets/plugin'];
 
   return {
     presets: [['babel-preset-expo', { jsxImportSource: 'nativewind' }], 'nativewind/babel'],
